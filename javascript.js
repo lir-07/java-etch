bfaa22bb0c6613e00f66158ba4e15ef66f751bd3
lir-07 <lirveseli08@gmail.com>
2026-07-06 11:28:58 +0200

    Created a function to allow for change of the squares inside the grid, added user input

--- javascript.js
+++ javascript.js
@@ -1,13 +1,43 @@
 const container = document.getElementById("container");
+const button = document.createElement("button");
+const input = document.getElementById("sizeInput");
 
-for(let i = 0; i < 256; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
+button.textContent = "Change The Grid!";
 
-    square.addEventListener("mouseenter", () => {
+function makeGridSize(number){
+    container.innerHTML = "";
+
+    const squareSize = 640 / number;
+
+    for(let i = 0; i < number*number; i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseenter", () => {
         square.style.backgroundColor = "black";
-    });
-
-    container.appendChild(square);
+        });
+        
+        container.appendChild(square);
+    }
 }
 
+
+
+button.addEventListener("click", () => {
+    let answer = prompt("Number of squares per side for the new grid?");
+
+    while (answer > 100 || answer < 0){
+        answer = prompt("Too big of a number! Number of squares per side for the new grid?");
+    }
+
+    makeGridSize(answer);
+
+
+})
+
+makeGridSize(16);
+
+document.body.append(button);
